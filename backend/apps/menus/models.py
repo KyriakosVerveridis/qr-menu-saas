a27046c735f937da5d9e7fc108640a034f44e5f9
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2026-05-02 00:41:59 +0300

    feat: add menu category model

--- backend/apps/menus/models.py
+++ backend/apps/menus/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.restaurants.models import Restaurant
 
-# Create your models here.
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=255)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="menu_categories")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
