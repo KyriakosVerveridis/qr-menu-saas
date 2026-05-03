2329b088e9896c88e8fae396f565842c4de66dc7
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2026-05-03 16:00:51 +0300

    feat: add grouped menu API with categories

--- backend/apps/menus/views.py
+++ backend/apps/menus/views.py
@@ -1,13 +1,34 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from apps.restaurants.models import Restaurant
-from .models import MenuItem
-from .serializers import PublicMenuItemSerializer
+from apps.restaurants.models import Restaurant
+from apps.menus.models import MenuCategory
+
 
 @api_view(['GET'])
 def public_menu(request, slug):
     restaurant = Restaurant.objects.get(slug=slug)
-    menu_items = MenuItem.objects.filter(restaurant=restaurant)
 
-    serializer = PublicMenuItemSerializer(menu_items, many=True)
-    return Response(serializer.data)
+    categories = MenuCategory.objects.filter(
+        restaurant=restaurant
+    ).prefetch_related("menu_items")
+
+    data = []
+
+    for category in categories:
+        items = []
+
+        for item in category.menu_items.all():
+            items.append({
+                "id": item.id,
+                "name": item.name_GR,
+                "price": item.price,
+                "image": item.image,
+            })
+
+        data.append({
+            "category": category.name,
+            "items": items
+        })
+
+    return Response(data)
