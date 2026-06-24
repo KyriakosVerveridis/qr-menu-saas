ee8a23706a10261da0bcab2207eacdc88b7be753
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2026-06-24 23:53:02 +0300

    feat: add user registration functionality

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserProfileView
+from .views import UserProfileView, RegisterView
 
 urlpatterns = [
     # Endpoints for Login and Token Refresh (from Simple JWT)
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
+    # Endpoint for Register
+    path('register/', RegisterView.as_view(), name='register'),
+
     # Endpoint for Profile
     path('profile/', UserProfileView.as_view(), name='user_profile'),
 ]
