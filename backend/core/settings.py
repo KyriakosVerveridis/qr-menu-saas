2be5efe2c88f965d1e4e46ab6f61e1480296d1bb
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2026-07-26 14:26:13 +0300

    Fix CORS allowed origins config

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -57,7 +57,10 @@ INSTALLED_APPS = [
     'apps.business_types',
 ]
 
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
+# CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
+CORS_ALLOWED_ORIGINS = [
+    origin.strip() for origin in os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',') if origin.strip()
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
